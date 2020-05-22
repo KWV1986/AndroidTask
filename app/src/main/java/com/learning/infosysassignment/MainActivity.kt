9d9f21eddb2946cf5ebd0e1556df282ed9bc6cf4
deepak <deepakg.borade@gmail.com>
2020-05-21 22:29:28 -0700

    Add Logs

--- app/src/main/java/com/learning/infosysassignment/MainActivity.kt
+++ app/src/main/java/com/learning/infosysassignment/MainActivity.kt
@@ -30,7 +30,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i("TAG","OnCreate")
+        Log.i("TAG","OnCreate")
         initviews()
+
 
     }
 
